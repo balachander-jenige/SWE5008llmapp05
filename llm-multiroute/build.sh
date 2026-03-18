3c0d0821c30a77f13e01e9ead2c135a290060208
balachander-jenige <eduwithbala@gmail.com>
2026-03-18 15:15:53 +0800

    changed the docker username to balachander20

--- llm-multiroute/build.sh
+++ llm-multiroute/build.sh
@@ -1,2 +1,2 @@
-  docker build --platform linux/arm64 -t darryl1975/llm-multiroute:latest .        
-  docker push darryl1975/llm-multiroute:latest 
+  docker build --platform linux/arm64 -t balachander20/llm-multiroute:latest .        
+  docker push balachander20/llm-multiroute:latest 
